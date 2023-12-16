a69db2a2505f19ff0a5612ced82979ea2e50a0b0
Sumit Tripathi <sumit.tpt07@gmail.com>
2023-12-16 23:16:34 +0530

    commit

--- Longest-Common-Prefix.py
+++ Longest-Common-Prefix.py
@@ -1,2 +1,15 @@
 # Input: strs = ["flower","flow","flight"]
 # Output: "fl"
+
+
+# strs = ["flower", "flow", "flight"]
+
+# words = sorted(strs)
+# ans = ""
+
+# for i in range(min(len(words[0]), len(words[-1]))):
+#     if words[0][i] != words[-1][i]:
+#         print(ans)
+#     ans = ans + words[0][i]
+
+# print(ans)
